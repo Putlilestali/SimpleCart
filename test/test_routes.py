5c1d3e17069d85ef12adecc17c081ee9d3cb20ad
putrilestari378 <putrilestari936@gmail.com>
2023-11-30 00:53:28 +0700

    first commit

---
+++ test/test_routes.py
@@ -0,0 +1,26 @@
+import json
+
+from .settings import DEFAULT_PRICE
+from api.utils import (
+    get_price,
+    get_subtotal,
+    apply_promo
+)
+
+
+def test_product_detail_api(client):
+    id = 3
+    response = client.get(f"/api/products/{id}")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert id == data.get('id')
+    assert DEFAULT_PRICE * id
+
+
+def test_product_api(client):
+    response = client.get("/api/products")
+    assert response.status_code == 200
+
+# post new cart
+def test_post_cart(client):
+     pass
